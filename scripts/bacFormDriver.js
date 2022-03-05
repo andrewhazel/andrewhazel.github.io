10d539cec4e0a4b0f107d549034163f4cb9aa7c2
Andrew Hazel <corvettehazel@yahoo.com>
2022-03-05 17:24:10 -0600

    Remediated naming error with bacRec.

--- scripts/bacFormDriver.js
+++ scripts/bacFormDriver.js
@@ -42,23 +42,23 @@ function bacFormDriver()
             if (bac == 0.0)
                 bacRec += "You are currently sober - you do not have alcohol in your bloodstream\n";
             else if (bac < 0.04 && bac > 0.00)
-                text += "No apparent effects are felt with this BAC. May experience elevated mood\n";
+                bacRec += "No apparent effects are felt with this BAC. May experience elevated mood\n";
             else if (bac >= 0.04 && bac < 0.07)
-                text += "May feel warm and relaxed - slight impairment in speech and memory.\n";
+                bacRec += "May feel warm and relaxed - slight impairment in speech and memory.\n";
             else if (bac >= 0.08 && bac < 0.10)
-                text += "Mild impairment of balance, speech, vision, and control. You are unable to legally operate a motor vehicle.\n";
+                bacRec += "Mild impairment of balance, speech, vision, and control. You are unable to legally operate a motor vehicle.\n";
             else if (bac >= 0.10 && bac < 0.13)
-                text += "Loss of judgement and moderate loss of motor control.\n";
+                bacRec += "Loss of judgement and moderate loss of motor control.\n";
             else if (bac >= 0.13 && bac < 0.16)
-                text += "Blurred vision and major loss of balance. May feel anxious or restless.\n";
+                bacRec += "Blurred vision and major loss of balance. May feel anxious or restless.\n";
             else if (bac >= 0.16 && bac < 0.20)
-                text += "Nausea is apparent - you may be considered 'sloppy drunk'.\n";
+                bacRec += "Nausea is apparent - you may be considered 'sloppy drunk'.\n";
             else if (bac >= 0.20 && bac < 0.35)
-                text += "Severe Intoxication - you may need help walking.\n";
+                bacRec += "Severe Intoxication - you may need help walking.\n";
             else if (bac >= 0.35 && bac < 0.40)
-                text += "Brink of comatose.\n";
+                bacRec += "Brink of comatose.\n";
             else
-                text += "Death is high possibility... seek immediate medical attention."
+                bacRec += "Death is high possibility... seek immediate medical attention."
             
             textArea.value = bacText;
             textArea.className = css;
